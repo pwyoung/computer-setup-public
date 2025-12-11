7b8a8ce0932406e285021d2e6bedf69e1ed5b92e
phillip young <phil.young@insight.com>
2025-12-10 20:17:43 -0500

    doc login shell

--- home/bin/login-shell.sh
+++ home/bin/login-shell.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# GOAL
+# - Make a shell that an IDE can use which will ensure
+#   that our login/setup stuff is run.
+
 # 1. Detect the user's default shell, or fallback to bash
 CURRENT_SHELL="${SHELL:-/bin/bash}"
 
